00bc05a816c68bd8ff62ad500c7727f29cb0b0b5
always <alwaysmeorg@qq.com>
2024-05-20 11:54:55 +0800

    update: 更新数据库迁移

--- database/migrations/1000_table_comment.php
+++ database/migrations/1000_table_comment.php
@@ -13,12 +13,12 @@ return new class extends Migration
         $table_prefix = env('DB_PREFIX', '');
         $arr = [
             'migrations' => '数据库迁移表',
+            'upload_record' => '资源上传记录表',
             'users' => '用户表',
             'permissions' => '权限菜单表',
             'permissions_role' => '角色所拥有的权限表',
             'role' => '角色表',
             'role_user' => '用户所属的角色表',
-            'upload_record' => '资源上传记录表',
         ];
         /* 生成表注释 */
         foreach ($arr as $k => $v) {
