1b6d9e7bb7fc44c7dc4d4ab11efb8a640802556a
always <alwaysmeorg@qq.com>
2024-05-21 16:25:01 +0800

    update: 更新菜单列表

--- packages/src/components/layout/Sidebar.vue
+++ packages/src/components/layout/Sidebar.vue
@@ -1,4 +1,8 @@
 <script setup>
+	const route = useRoute()
+	const router = useRouter()
+
+	const treeMenuRef = ref([])
 	const sidebar_list = reactive([
 		{
 			id: 1,
@@ -30,6 +34,20 @@
 			]
 		}
 	])
+
+	const expanded_keys = computed(() => {
+		return route.matched
+			.filter((item) => item.name)
+			.map((item) => item.name)
+			.slice(0, 2)
+	})
+	const expanded_keys_highlight = computed(() => {
+		return expanded_keys.value[expanded_keys.value.length - 1]
+	})
+
+	function handleMenu(data, node) {
+		router.push({ name: data.code })
+	}
 </script>
 
 <template>
@@ -38,14 +56,25 @@
 			:data="sidebar_list"
 			:props="{ children: 'children', label: 'name' }"
 			:node-height="50"
+			:indent="36"
+			:default-expanded-keys="expanded_keys"
+			:default-expanded-keys-highlight="expanded_keys_highlight"
+			ref="treeMenuRef"
+			node-key="code"
 			ellipsis
 			menu-collapsible
-		></tiny-tree-menu>
+			only-check-children
+			@nodeClick="handleMenu"
+		/>
 	</div>
 </template>
 
 <style scoped lang="scss">
 	.sidebar {
+		:deep(.tiny-input__inner) {
+			height: 40px;
+		}
+
 		:deep(.tiny-tree-menu) {
 			transition: width 0.3s;
 			height: calc(100vh - 60px);
