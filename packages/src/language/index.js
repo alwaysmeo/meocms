b7aea8885f9b97538f2f2af3d4f3823dea3c581a
always <alwaysmeorg@qq.com>
2024-06-25 18:30:03 +0800

    update: 更新i18n国际化

--- packages/src/language/index.js
+++ packages/src/language/index.js
@@ -3,8 +3,8 @@ import locale from '@opentiny/vue-locale'
 import enUS from './en-US.json'
 import zhCN from './zh-CN.json'
 
-export default (i18n) => {
-	return locale.initI18n({
+export default (i18n) =>
+	locale.initI18n({
 		i18n,
 		createI18n: ({ locale, messages }) => {
 			return createI18n({
@@ -13,15 +13,5 @@ export default (i18n) => {
 				legacy: false
 			})
 		},
-		messages: {
-			zhCN: {
-				test: '中文',
-				meo: zhCN
-			},
-			enUS: {
-				test: 'English',
-				meo: enUS
-			}
-		}
+		messages: { zhCN, enUS }
 	})
-}
