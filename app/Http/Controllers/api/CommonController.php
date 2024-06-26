ec68e7ad3ed5bde2a77140a4bfc293d5dbc12e17
always <alwaysmeorg@qq.com>
2024-06-26 14:01:23 +0800

    update: 优化验证码生成方法

--- app/Http/Controllers/api/CommonController.php
+++ app/Http/Controllers/api/CommonController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Mews\Captcha\Facades\Captcha;
 
 class CommonController extends Controller
 {
 	public function captcha(Request $request): Response
 	{
-		$data = Captcha::create('default', true);
+		$req = $request->only(['type']);
+		$validator = Validator::make($req, ['type' => Rule::in(['default', 'math', 'flat', 'mini', 'inverse'])]);
+		if (!$validator->passes()) return $this->fail(null, $validator->errors()->first(), 5000);
+		$data = Captcha::create($req['type'] ?? 'math', true);
 		return $this->success($data);
 	}
 }
