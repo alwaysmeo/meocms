f67a76890ae14afd55c01a46c2220fb4ff82850e
always <alwaysmeorg@qq.com>
2024-07-03 12:35:31 +0800

    update: 更新 i18n 语言

--- packages/src/App.vue
+++ packages/src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
 	import i18n from '@language'
-	import zhCN from 'ant-design-vue/es/locale/zh_CN'
-	import zhTW from 'ant-design-vue/es/locale/zh_TW'
+	import deDE from 'ant-design-vue/es/locale/de_DE'
 	import enUS from 'ant-design-vue/es/locale/en_US'
 	import frFR from 'ant-design-vue/es/locale/fr_FR'
-	import deDE from 'ant-design-vue/es/locale/de_DE'
 	import jaJP from 'ant-design-vue/es/locale/ja_JP'
 	import koKR from 'ant-design-vue/es/locale/ko_KR'
 	import ruRU from 'ant-design-vue/es/locale/ru_RU'
+	import zhCN from 'ant-design-vue/es/locale/zh_CN'
+	import zhTW from 'ant-design-vue/es/locale/zh_TW'
 
 	const language = {
-		zhCN: zhCN,
-		zhTW: zhTW,
+		deDE: deDE,
 		enUS: enUS,
 		frFR: frFR,
-		deDE: deDE,
 		jaJP: jaJP,
 		koKR: koKR,
-		ruRU: ruRU
+		ruRU: ruRU,
+		zhCN: zhCN,
+		zhTW: zhTW
 	}
 </script>
 
--- packages/src/language/index.js
+++ packages/src/language/index.js
@@ -1,12 +1,24 @@
 import { createI18n } from 'vue-i18n'
+import deDE from './de_DE.json'
 import enUS from './en_US.json'
+import frFR from './fr_FR.json'
+import jaJP from './ja_JP.json'
+import koKR from './ko_KR.json'
+import ruRU from './ru_RU.json'
 import zhCN from './zh_CN.json'
+import zhTW from './zh_TW.json'
 
 export default createI18n({
-	locale: 'enUS',
+	locale: 'zhCN',
 	messages: {
+		deDE,
+		enUS,
+		frFR,
+		jaJP,
+		koKR,
+		ruRU,
 		zhCN,
-		enUS
+		zhTW
 	},
 	globalInjection: true,
 	legacy: false
