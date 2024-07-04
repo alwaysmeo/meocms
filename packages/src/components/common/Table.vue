88a2875218aa8a7c79d7c836a83f5dda9b025893
always <alwaysmeorg@qq.com>
2024-07-04 19:37:04 +0800

    add: 新增表格组件

--- packages/src/components/common/Table.vue
+++ packages/src/components/common/Table.vue
@@ -1,7 +1,85 @@
+<!-- 表格组件 -->
 <script setup>
-	const titel = ref('表格')
+	import { isEqual, first, omit } from 'radash'
+
+	const emits = defineEmits(['paginate', 'action'])
+
+	const props = defineProps({
+		total: {
+			type: Number,
+			default: 0
+		},
+		action: {
+			type: Object,
+			default: () => ({})
+		}
+	})
+
+	const pagination = reactive({
+		current: 1,
+		pageSize: 10
+	})
+
+	const action_first = computed(() => {
+		return {
+			key: first(Object.keys(props.action)),
+			value: first(Object.values(props.action))
+		}
+	})
+	const action_list = computed(() => {
+		return omit(props.action, [action_first.value.key])
+	})
+
+	function change(paginate, filters, sorter, { action, currentDataSource }) {
+		Object.assign(pagination, paginate)
+		emits(
+			action,
+			{
+				page: pagination.current,
+				limit: pagination.pageSize,
+				total: props.total
+			},
+			filters,
+			sorter,
+			{
+				action,
+				currentDataSource
+			}
+		)
+	}
+
+	function handleAction(key, record) {
+		emits('action', key, record)
+	}
 </script>
 
 <template>
-	<div>{{ titel }}</div>
+	<a-table class="custom-table" @change="change" :pagination="{ ...pagination, total: props.total }" v-bind="$attrs">
+		<template #bodyCell="scoped">
+			<template v-if="isEqual(scoped.column.dataIndex, 'index')">
+				{{ (pagination.current - 1) * pagination.pageSize + (scoped.index + 1) }}
+			</template>
+			<slot name="bodyCell" v-bind="scoped"></slot>
+			<template v-if="isEqual(scoped.column.dataIndex, 'action')">
+				<a-space>
+					<a-button
+						class="color-primary"
+						type="text"
+						size="small"
+						@click="handleAction(action_first.key, scoped.record)"
+					>
+						{{ action_first.value }}
+					</a-button>
+					<meo-dropdown :list="action_list" @menu="handleAction($event, scoped.record)" />
+				</a-space>
+			</template>
+		</template>
+		<template #emptyText>
+			<div class="text-align-center">{{ $attrs.emptyTxt ?? '暂无数据' }}</div>
+		</template>
+	</a-table>
 </template>
+
+<style scoped lang="scss">
+	/** table */
+</style>
