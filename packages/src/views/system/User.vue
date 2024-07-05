7f2f9b189ebed1be27ece6b00869679447f6f839
always <alwaysmeorg@qq.com>
2024-07-05 17:18:27 +0800

    update: 更新用户管理页

--- packages/src/views/system/User.vue
+++ packages/src/views/system/User.vue
@@ -10,9 +10,14 @@
 	const table = reactive({
 		columns: [
 			{ dataIndex: 'index', title: t('meo.pages.system.user.table.columns.index'), width: 120, align: 'center', show: true },
+			{ dataIndex: 'ulid', key: 'ulid', title: 'ID', width: 260 },
+			{ dataIndex: 'picture_info', key: 'picture_info', title: '用户头像', width: 120, align: 'center', show: true },
 			{ dataIndex: 'email', key: 'email', title: t('meo.pages.system.user.table.columns.email'), show: true },
+			{ dataIndex: 'phone', key: 'phone', title: '联系电话', width: 150, align: 'center' },
 			{ dataIndex: 'nickname', key: 'nickname', title: t('meo.pages.system.user.table.columns.nickname'), show: true },
-			{ dataIndex: 'role', key: 'role', title: t('meo.pages.system.user.table.columns.role'), show: true },
+			{ dataIndex: 'role', key: 'role', title: t('meo.pages.system.user.table.columns.role'), align: 'center', show: true },
+			{ dataIndex: 'created_at', key: 'created_at', title: '注册时间', width: 170, align: 'center' },
+			{ dataIndex: 'last_login_at', key: 'last_login_at', title: '最后一次登录时间', width: 170, align: 'center' },
 			{ dataIndex: 'status', key: 'status', title: t('meo.pages.system.user.table.columns.status'), width: 120, align: 'center', show: true },
 			{ dataIndex: 'action', key: 'action', title: t('meo.pages.system.user.table.columns.action'), width: 160, align: 'center', show: true }
 		],
@@ -58,7 +63,7 @@
 			</div>
 			<meo-table
 				v-model:open="table.open"
-				:columns="table.columns"
+				v-model:columns="table.columns"
 				:dataSource="table.data"
 				:loading="table.loading"
 				:total="table.total"
@@ -69,6 +74,14 @@
 				<template #bodyCell="{ column, record }">
 					<template v-if="isEqual(column.dataIndex, 'role')">
 						{{ record.role ?? '未设置' }}
+					</template>
+					<template v-if="isEqual(column.dataIndex, 'picture_info')">
+						<a-avatar :size="50" :src="record?.picture_info?.url">
+							<template #icon><ant-user-outlined /></template>
+						</a-avatar>
+					</template>
+					<template v-if="isEqual(column.dataIndex, 'phone')">
+						{{ record.phone ?? '-' }}
 					</template>
 				</template>
 			</meo-table>
