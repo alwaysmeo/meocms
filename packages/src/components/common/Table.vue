13b40e91eb5b097e28cdcb696e5678d96947b3b8
always <alwaysmeorg@qq.com>
2024-07-05 13:46:56 +0800

    update: 更新表格组件

--- packages/src/components/common/Table.vue
+++ packages/src/components/common/Table.vue
@@ -1,8 +1,9 @@
 <!-- 表格组件 -->
 <script setup>
 	import { isEqual, first, omit } from 'radash'
+	import { useVModel } from '@vueuse/core'
 
-	const emits = defineEmits(['paginate', 'action'])
+	const emits = defineEmits(['update:open', 'paginate', 'action'])
 
 	const props = defineProps({
 		columns: {
@@ -21,35 +22,31 @@
 			default: () => new Object(),
 			message: '操作列中的数据对象：{ key: value }'
 		},
-		selector: {
+		open: {
 			type: Boolean,
 			default: false,
-			message: '表头选择器'
+			message: '控制选择可见的表格列的模态框显示隐藏 v-model:open'
 		}
 	})
 
-	const pagination = reactive({
-		current: 1,
-		pageSize: 10
-	})
-
-	const action_first = computed(() => {
-		return {
-			key: first(Object.keys(props.action)),
-			value: first(Object.values(props.action))
-		}
-	})
-	const action_list = computed(() => {
-		return omit(props.action, [action_first.value.key])
+	const state = reactive({
+		page: 1,
+		limit: 10,
+		open: useVModel(props, 'open', emits),
+		action_first: computed(
+			() => new Object({ key: first(Object.keys(props.action)), value: first(Object.values(props.action)) })
+		),
+		action_list: computed(() => omit(props.action, [state.action_first.value.key]))
 	})
 
 	function change(paginate, filters, sorter, { action, currentDataSource }) {
-		Object.assign(pagination, paginate)
+		state.page = paginate.current
+		state.limit = paginate.pageSize
 		emits(
 			action,
 			{
-				page: pagination.current,
-				limit: pagination.pageSize,
+				page: state.page,
+				limit: state.limit,
 				total: props.total
 			},
 			filters,
@@ -64,42 +61,62 @@
 	function handleAction(key, record) {
 		emits('action', key, record)
 	}
+
+	function confirm() {
+		console.log(props.columns)
+	}
 </script>
 
 <template>
-	<a-table
-		class="custom-table"
-		@change="change"
-		:pagination="{ ...pagination, total: props.total }"
-		:scroll="{ x: 1200 }"
-		v-bind="$attrs"
-		:columns="props.columns"
-	>
-		<template #bodyCell="scoped">
-			<template v-if="isEqual(scoped.column.dataIndex, 'index')">
-				{{ (pagination.current - 1) * pagination.pageSize + (scoped.index + 1) }}
+	<div class="meo-table-container">
+		<a-table
+			class="meo-table"
+			@change="change"
+			:pagination="{ current: state.page, pageSize: state.limit, total: props.total }"
+			:scroll="{ x: 1200 }"
+			v-bind="$attrs"
+			:columns="props.columns"
+		>
+			<template #bodyCell="scoped">
+				<template v-if="isEqual(scoped.column.dataIndex, 'index')">
+					{{ (state.page - 1) * state.limit + (scoped.index + 1) }}
+				</template>
+				<slot name="bodyCell" v-bind="scoped"></slot>
+				<template v-if="isEqual(scoped.column.dataIndex, 'action')">
+					<a-space>
+						<a-button
+							class="color-primary"
+							type="text"
+							size="small"
+							@click="handleAction(state.action_first.key, scoped.record)"
+						>
+							{{ state.action_first.value }}
+						</a-button>
+						<meo-dropdown :list="state.action_list" @menu="handleAction($event, scoped.record)" />
+					</a-space>
+				</template>
 			</template>
-			<slot name="bodyCell" v-bind="scoped"></slot>
-			<template v-if="isEqual(scoped.column.dataIndex, 'action')">
-				<a-space>
-					<a-button
-						class="color-primary"
-						type="text"
-						size="small"
-						@click="handleAction(action_first.key, scoped.record)"
-					>
-						{{ action_first.value }}
-					</a-button>
-					<meo-dropdown :list="action_list" @menu="handleAction($event, scoped.record)" />
-				</a-space>
+			<template #emptyText>
+				<div class="text-align-center">{{ $attrs.emptyTxt ?? '暂无数据' }}</div>
 			</template>
-		</template>
-		<template #emptyText>
-			<div class="text-align-center">{{ $attrs.emptyTxt ?? '暂无数据' }}</div>
-		</template>
-	</a-table>
+		</a-table>
+		<a-modal v-model:open="state.open" title="选择可见的表格列" centered @ok="confirm" @cancel="state.open = false">
+			<p>Some contents...</p>
+			<p>Some contents...</p>
+			<p>Some contents...</p>
+		</a-modal>
+	</div>
 </template>
 
 <style scoped lang="scss">
-	/** table */
+	.meo-table-container {
+		position: relative;
+		.selector {
+			position: absolute;
+			top: 0;
+			left: 0;
+			z-index: 1;
+			font-size: 20px;
+		}
+	}
 </style>
