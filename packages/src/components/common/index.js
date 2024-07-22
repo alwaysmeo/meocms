d0cd1dd8250e681cd88ab20ebdbf44288f2c9f88
always <alwaysmeorg@qq.com>
2024-07-22 18:19:07 +0800

    add: 添加 Image 公共组件

--- packages/src/components/common/index.js
+++ packages/src/components/common/index.js
@@ -1,5 +1,6 @@
 import Dropdown from './Dropdown.vue'
+import Image from './Image.vue'
 import Modal from './Modal.vue'
 import Table from './Table.vue'
 
-export { Dropdown, Modal, Table }
+export { Dropdown, Image, Modal, Table }
