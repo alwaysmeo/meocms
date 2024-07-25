16b35d1991150b7a3abf8541da58f183b9571cb5
always <alwaysmeorg@qq.com>
2024-07-25 12:25:22 +0800

    update: 优化表排序字段

--- database/migrations/0003_roles.php
+++ database/migrations/0003_roles.php
@@ -15,8 +15,8 @@ return new class extends Migration {
 			$table->id();
 			$table->string('name', 30)->comment('角色名称');
 			$table->string('description', 200)->nullable()->comment('角色描述');
-			$table->integer('slot')->default(0)->comment('角色排序');
 			$table->tinyInteger('show')->default(1)->comment('是否启用【0:关闭, 1:开启】');
+			$table->integer('slot')->nullable()->comment('角色排序');
 			$table->dateTime('created_at')->useCurrent()->comment('创建时间');
 			$table->dateTime('updated_at')->nullable()->useCurrentOnUpdate()->comment('更新时间');
 			$table->dateTime('deleted_at')->nullable()->comment('删除时间');
--- database/migrations/0004_permission.php
+++ database/migrations/0004_permission.php
@@ -20,9 +20,9 @@ return new class extends Migration
             $table->string('description', 200)->nullable()->comment('权限描述');
             $table->string('icon', 100)->nullable()->comment('权限图标');
             $table->string('path', 255)->comment('页面路径地址');
-            $table->integer('slot')->default(0)->comment('菜单排序');
             $table->tinyInteger('level')->default(1)->comment('菜单层级 【1:一级, 2:二级, 3:三级】');
             $table->tinyInteger('show')->default(1)->comment('是否显示【0:隐藏, 1:显示】');
+            $table->integer('slot')->nullable()->comment('菜单排序');
 	        $table->dateTime('created_at')->useCurrent()->comment('创建时间');
 	        $table->dateTime('updated_at')->nullable()->useCurrentOnUpdate()->comment('更新时间');
 	        $table->dateTime('deleted_at')->nullable()->comment('删除时间');
